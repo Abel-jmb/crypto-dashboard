49fad18686232f9828c39555ff69ad97f3f210fb
Abel Montaño <abelmontap18@gmail.com>
2026-04-07 21:51:02 -0500

    style: add styles to header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import CoinList from "./components/coinList"
+import Header from "./components/Header"
 import { API_URL } from "./constants/api"
 import useFetch from "./hooks/useFetch"
 
@@ -10,7 +11,10 @@ function App() {
 
   return (
     <>
+    <main className="min-h-screen max-w-md mx-auto px-4 dark:bg-gray-900 dark:text-amber-50">
+    <Header />
     <CoinList data={data} loading={loading} error={error}/>
+    </main>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import { useTheme } from "../contexts/themeContext";
+
+const Header = () => {
+  const { theme, handleTheme } = useTheme();
+
+  return (
+    <header className="p-6 dark:bg-gray-900 dark:text-amber-50">
+      <div className="flex justify-between">
+        <h1 className="text-lg font-black tracking-tigh">Crypto Dashboard</h1>
+        <div className="flex items-center justify-between gap-4">
+          <label className="relative inline-flex h-6 w-12 cursor-pointer items-center">
+            <input
+              type="checkbox"
+              onChange={handleTheme}
+              className="peer sr-only"
+            />
+            <div className="absolute inset-0 rounded-full bg-gray-300 transition-colors duration-300 peer-checked:bg-linear-to-r peer-checked:from-[#378fe6] peer-checked:to-[#3eda82]"></div>
+            <div className="absolute left-1 h-4 w-4 rounded-full bg-white transition-transform duration-300 peer-checked:translate-x-6 dark:bg-slate-900"></div>
+          </label>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
