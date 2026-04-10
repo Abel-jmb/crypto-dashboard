e5adef085ad4a4f267c09bbe73cc8ff8c5f3f72b
Abel Montaño <abelmontap18@gmail.com>
2026-04-10 13:27:30 -0500

    feat: implement search functionality in SearchBar

--- src/components/CoinList.jsx
+++ src/components/CoinList.jsx
@@ -2,19 +2,20 @@ import CoinCard from "./CoinCard";
 import Error from "./Error";
 import Loader from "./Loader";
 
-const CoinList = ({data, loading, error}) => {
+const CoinList = ({ data, loading, error, search }) => {
+  
+    let coinSearch = data && data.filter((c) => c.name.toLowerCase().includes(search.toLowerCase()));
 
-    return (
-        <div>
-            {loading && <Loader />}
-            {error && <Error error={error}/>}
-            <ul className="grid grid-cols-1 gap-4">
-                {data && data.map(c => (
-                    <CoinCard key={c.id} coin={c}/>
-                ))}
-            </ul>
-        </div>
-    );
-}
- 
+
+  return (
+    <div>
+      {loading && <Loader />}
+      {error && <Error error={error} />}
+      <ul className="grid grid-cols-1 gap-4">
+        {coinSearch && coinSearch.map((c) => <CoinCard key={c.id} coin={c} />)}
+      </ul>
+    </div>
+  );
+};
+
 export default CoinList;
